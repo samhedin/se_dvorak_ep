df4e6302d7fd05c83db6fa0a0806c6713fdd0ded
Sam Hedin <sam.hedin@gmail.com>
2020-07-13 14:00:59 +0200

    Crop

--- update_imgs.sh
+++ update_imgs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-xkbprint -ll 1 :0.0 -o layout1.ps; gs -dSAFER -dEPSCrop -r600 -sDEVICE=pngalpha -o layout1.png layout1.ps; mogrify -scale 50% -trim -fuzz 50% -flatten -rotate "-90" layout1.png; rm layout1.ps;
-xkbprint -ll 2 :0.0 -o layout2.ps; gs -dSAFER -dEPSCrop -r600 -sDEVICE=pngalpha -o layout2.png layout2.ps; mogrify -scale 50% -trim -fuzz 50% -flatten -rotate "-90" layout2.png; rm layout2.ps;
+xkbprint -ll 1 :0.0 -o layout1.ps; gs -dSAFER -dEPSCrop -r600 -sDEVICE=pngalpha -o layout1.png layout1.ps
+mogrify -scale 25% -crop 550x1000+350+100 -rotate "-90" -flatten layout1.png
+rm layout1.ps
+
+xkbprint -ll 2 :0.0 -o layout2.ps; gs -dSAFER -dEPSCrop -r600 -sDEVICE=pngalpha -o layout2.png layout2.ps
+mogrify -scale 25% -crop 550x1000+350+100 -rotate "-90" -flatten layout2.png
+rm layout2.ps
